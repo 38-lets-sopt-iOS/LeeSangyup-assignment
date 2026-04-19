2c22bdba81c9f2dc471258cc53b754526af6f2ba
sangyup <sangyeobi712@gmail.com>
2026-04-19 10:03:41 +0900

    [#1] Font 추가

---
+++ WATCHA_Clone/WATCHA_Clone/Global/Extension/UIFont+.swift
@@ -0,0 +1,25 @@
+//
+//  UIFont+.swift
+//  WATCHA_Clone
+//
+//  Created by 이상엽 on 4/18/26.
+//
+
+import UIKit
+
+extension UIFont {
+    static let head1 = UIFont(name: "Pretendard-SemiBold", size: 30)!
+    static let head2 = UIFont(name: "Pretendard-SemiBold", size: 23)!
+    static let head3 = UIFont(name: "Pretendard-SemiBold", size: 20)!
+    
+    static let subhead1 = UIFont(name: "Pretendard-SemiBold", size: 18)!
+    static let subhead2 = UIFont(name: "Pretendard-SemiBold", size: 15)!
+    static let subhead3 = UIFont(name: "Pretendard-SemiBold", size: 12)!
+
+    static let medium = UIFont(name: "Pretendard-Medium", size: 18)!
+    
+    static let body1 = UIFont(name: "Pretendard-Medium", size: 12)!
+    static let body2 = UIFont(name: "Pretendard-Regular", size: 12)!
+
+    static let cap1 = UIFont(name: "Pretendard-Light", size: 12)!
+}
